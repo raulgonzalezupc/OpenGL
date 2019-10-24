7748906131357775061835ec83b7c5ddfc84312b
raulgonzalezupc <56253770+raulgonzalezupc@users.noreply.github.com>
2019-10-24 21:26:37 +0200

    Adding MathGeoLib

--- LittleEngine/Globals.h
+++ LittleEngine/Globals.h
@@ -19,3 +19,4 @@ enum update_status
 #define FULLSCREEN false
 #define VSYNC true
 #define TITLE "Super Awesome Engine"
+#define PI 3.14159265359
--- LittleEngine/ModuleRender.cpp
+++ LittleEngine/ModuleRender.cpp
@@ -5,6 +5,11 @@
 #include "Application.h"
 #include "SDL.h"
 #include "glew-2.1.0/include/GL/glew.h"
+#include "MathGeoLib/include/Math/float4.h"
+#include "MathGeoLib/include/Math/float3.h"
+#include "MathGeoLib/include/Math/float4x4.h"
+#include "MathGeoLib/include/Math/float3x3.h"
+#include "MathGeoLib/include/Geometry/Frustum.h"
 
 ModuleRender::ModuleRender()
 {
@@ -42,7 +47,47 @@ bool ModuleRender::Init()
 	float buffer_data[] = { -1.0f, -1.0f, 0.0f,
 							 1.0f, -1.0f, 0.0f,       
 							 0.0f,  1.0f, 0.0f };
-	GLuint vbo; glGenBuffers(1, &vbo); 
+
+
+	float aspect = SCREEN_WIDTH / SCREEN_HEIGHT;
+	Frustum frustum;
+	frustum.type = FrustumType::PerspectiveFrustum;
+	frustum.pos = math::float3::zero; 
+	frustum.front = -math::float3::unitZ;
+	frustum.up = math::float3::unitY;
+	frustum.nearPlaneDistance = 0.1f;
+	frustum.farPlaneDistance = 100.0f;
+	frustum.verticalFov = PI / 4.0f;
+	frustum.horizontalFov = 2.f * atanf(tanf(frustum.verticalFov * 0.5f) * aspect);
+	math::float4x4 proj = frustum.ProjectionMatrix();
+	math::float4x4 model = math::float4x4::FromTRS(math::float3(0.0f, 0.0f, -4.0f), math::float3x3::RotateY(PI / 4.0f), math::float3(1.0f, 1.0f, 1.0f));
+	float4x4 view = float4x4::LookAt(math::float3(0.0f,0.0f,-1.0f), math::float3(0.0f, 0.0f, -1.0f), math::float3(0.0f, 1.0f, 0.0f), math::float3(0.0f, 1.0f, 0.0f));
+	math::float4x4 transform = proj * view * float4x4(model);
+
+	//translation
+	float4 vertex0(buffer_data[0], buffer_data[1], buffer_data[2], 1.0f);
+	float4 tmp0 = transform * vertex0;
+	float3 finalVertex0 (tmp0.x / tmp0.w, tmp0.y / tmp0.w, tmp0.z / tmp0.w);
+	buffer_data[0] = finalVertex0.x;
+	buffer_data[1] = finalVertex0.y;
+	buffer_data[2] = finalVertex0.z;
+
+	float4 vertex1(buffer_data[3], buffer_data[4], buffer_data[5], 1.0f);
+	float4 tmp1 = transform * vertex1;
+	float3 finalVertex1(tmp1.x / tmp1.w, tmp1.y / tmp1.w, tmp1.z / tmp1.w);
+	buffer_data[3] = finalVertex1.x;
+	buffer_data[4] = finalVertex1.y;
+	buffer_data[5] = finalVertex1.z;
+
+	float4 vertex2(buffer_data[6], buffer_data[7], buffer_data[8], 1.0f);
+	float4 tmp2 = transform * vertex2;
+	float3 finalVertex2(tmp2.x / tmp2.w, tmp2.y / tmp2.w, tmp2.z / tmp2.w);
+	buffer_data[6] = finalVertex2.x;
+	buffer_data[7] = finalVertex2.y;
+	buffer_data[8] = finalVertex2.z;
+
+	GLuint vbo; 
+	glGenBuffers(1, &vbo); 
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(buffer_data), buffer_data, GL_STATIC_DRAW); 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
